fe9ed64713f13acbb36fc315ec6b9f74d4e23b1d
伊東和成 <itokazunari5375@icloud.com>
2022-04-05 23:20:09 +0900

    円グラフ

---
+++ src/components/PiePlot.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+// 円グラフとドーナッツデータ
+import { Pie, Doughnut } from "react-chartjs-2";
+
+export const PiePlot = () => {
+  const data = {
+    labels: ["windows", "mac", "linux"],
+    datasets: [
+      {
+        // %で表記
+        data: [90, 7, 3],
+        // 背景色
+        backgroundColor: ["#4169e1", "#ff1493", "#FFCE56"],
+        // ホバー時の色
+        hoverBackgroundColor: ["#36A2EB", "#FF6384", "#FFCE56"],
+        borderColor: ["transparent", "transparent", "transparent"],
+      },
+    ],
+  };
+  return <Pie data={data}></Pie>;
+};
